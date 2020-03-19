29e2482f5bfebbeb2ab2275a153bfd11d79f5844
Vedip <62332717+Vedip@users.noreply.github.com>
2020-03-19 06:28:43 +0100

    Add files via upload
    NO changes made

---
+++ While loop.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vendip
+ * Date: 3/19/2020
+ * Time: 6:09 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace While_loop
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int i=1;
+			while (i<=5)
+				Console.WriteLine("The loop ends");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
